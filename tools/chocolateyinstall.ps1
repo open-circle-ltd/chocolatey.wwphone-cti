42a00f714c8889668eacfd7c74bc6078d36b500b
bmcatherasoo <benaiahcatherasoo@gmail.com>
2021-12-08 12:12:49 +0100

    Add Chocmilk function

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,14 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://wwcom.ch/downloads/cti_3_4_10.exe' 
+$urlPackage        = 'https://wwcom.ch/downloads/cti_3_4_10.exe' 
+$checksumPackage   = '65151C1A032A49285AF597A89696B9FD491ACF6B2324322A5BBC4C3D2177E555'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
+  url           = $urlPackage
   softwareName  = 'cti*'
-  checksum      = '65151C1A032A49285AF597A89696B9FD491ACF6B2324322A5BBC4C3D2177E555'
+  checksum      = $checksumPackage
   checksumType  = 'sha256'
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
