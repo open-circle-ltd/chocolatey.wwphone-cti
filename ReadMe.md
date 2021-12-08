3b16a5ab0feda4f3fc70915d4c5c5ff8a194e93d
bmcatherasoo <benaiahcatherasoo@gmail.com>
2021-12-08 11:47:09 +0100

    Initial Commit

---
+++ ReadMe.md
@@ -0,0 +1,22 @@
+﻿# Chocolatey Package: wwphone CTI-Client
+
+## Description
+
+wwphone is a Softphone Solution from wwcom, this is the CTI-Client
+
+## Installation
+
+```ps1
+choco install wwphone
+```
+
+
+## Author
+
+- Benaiah Matthew Catherasoo
+- [Open Circle AG](https://www.open-circle.ch)
+
+
+## Copyright
+
+(c) 2021, Open Circle AG
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://wwcom.ch/downloads/cti_3_4_10.exe' 
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'cti*'
+  checksum      = '65151C1A032A49285AF597A89696B9FD491ACF6B2324322A5BBC4C3D2177E555'
+  checksumType  = 'sha256'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
